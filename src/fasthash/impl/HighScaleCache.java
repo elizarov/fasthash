86f6b4c8da459a8df2f1cde07c7c75c1521c4b1d
elizarov <elizarov@gmail.com>
2012-05-18 15:35:50 +0400

    Print fill-factor for high-scale-cache

--- src/fasthash/impl/HighScaleCache.java
+++ src/fasthash/impl/HighScaleCache.java
@@ -1,5 +1,8 @@
 package fasthash.impl;
 
+import java.lang.reflect.Field;
+import java.util.Locale;
+
 import fasthash.model.Cache;
 import fasthash.model.Order;
 import org.cliffc.high_scale_lib.NonBlockingHashMapLong;
@@ -20,6 +23,21 @@ public class HighScaleCache implements Cache {
 	}
 
 	public String describe() {
-		return getClass().getSimpleName();
+		// We get into private fields to figure out hash capacity and to compute its fill factor.
+		int capacity;
+		try {
+			Field chmField = NonBlockingHashMapLong.class.getDeclaredField("_chm");
+			chmField.setAccessible(true);
+			Object chm = chmField.get(map);
+			Field valsField = chm.getClass().getDeclaredField("_vals");
+			valsField.setAccessible(true);
+			Object[] vals = (Object[])valsField.get(chm);
+			capacity = vals.length;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return String.format(Locale.US, "%s %.2f%%",
+			getClass().getSimpleName(),
+			100.0 * map.size() / capacity);
 	}
 }
